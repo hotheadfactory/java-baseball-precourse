2895eb55489aa32fbdbe346c7f46a141bf2e45f1
Andy H. Jung <commaniakr@gmail.com>
2020-03-09 19:39:50 +0900

    feat: 아무것도 일치하지 않 때 낫싱을 출력하도록 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,8 +3,29 @@ package baseball.view;
 import java.util.Map;
 
 public class OutputView {
+    public static final String NOTHING = "낫싱";
+    public static final String STRIKE = "스트라이크 ";
+    public static final String BALL = "볼";
+
     public static void printResult(Map<String, Integer> result) {
-        System.out.printf("%d스트라이크 %d볼\n\n", result.get("Strike"), result.get("Ball"));
+        int strike = result.get("Strike");
+        int ball = result.get("Ball");
+
+        System.out.println(makeResultMessage(strike, ball));
+    }
+
+    private static String makeResultMessage(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+        if (strike == 0 && ball == 0) {
+            return NOTHING;
+        }
+        if (strike > 0) {
+            sb.append(strike).append(STRIKE);
+        }
+        if (ball > 0) {
+            sb.append(ball).append(BALL);
+        }
+        return sb.toString();
     }
 
     public static void printCorrect() {
