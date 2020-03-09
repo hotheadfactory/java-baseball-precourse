a71c2386baf8ec1caf421a849dc0e051c1170834
Andy H. Jung <commaniakr@gmail.com>
2020-03-09 16:46:55 +0900

    feat: BaseballNumbers가 null일때 NullPointerException 발생

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,12 +1,14 @@
 package baseball.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class BaseballNumbers {
-    public static final int SIZE = 6;
+    public static final int SIZE = 3;
     private final List<BaseballNumber> baseballNumbers;
 
     public BaseballNumbers(List<BaseballNumber> baseballNumbers) {
+        Objects.requireNonNull(baseballNumbers);
         if (baseballNumbers.size() != SIZE) {
             throw new IllegalArgumentException("숫자 갯수가 올바르지 않습니다.");
         }
